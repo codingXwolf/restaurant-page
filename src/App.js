061a7485655df69e5070145f7107662c7cd1f483
Jaime Ortiz <jxme88@gmail.com>
2021-04-05 22:31:39 -0700

    Playing with antd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import { Layout } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> What is up puto!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header />
+      </Layout>
     </div>
   );
 }
