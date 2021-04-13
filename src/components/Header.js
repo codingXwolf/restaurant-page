b3101ffe03b4c79dc489c9d177adfbddbed84e38
Jaime Ortiz <jxme88@gmail.com>
2021-04-12 19:22:31 -0700

    Added menu and title to the right. Menu not working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
-const Header = () => {
-    return (
-        <>
-           <h1 className="restaurant-title">Odin Sushi</h1> 
-           
-        </>
-    )
-}
+import { Menu, Segment } from "semantic-ui-react";
 
-export default Header
+const Header = () => {
+
+  return (
+    <>
+      <Segment inverted>
+        <Menu inverted pointing secondary stackable>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
+          <Menu.Item name="friends" />
+
+          <Menu.Item position="right">
+            <h1 className="restaurant-title">Odin Sushi</h1>
+          </Menu.Item>
+        </Menu>
+      </Segment>
+    </>
+  );
+};
+
+export default Header;
