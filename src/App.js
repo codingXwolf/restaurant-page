ccad32b9a2759feca871feb7596eb127149476d3
Jaime Ortiz <jxme88@gmail.com>
2021-04-19 18:43:42 -0700

    Added basic footer. Will fix style later

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "semantic-ui-css/semantic.min.css";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Menu from "./components/Menu";
+// import Menu from "./components/Menu";
+import { Menu, Input } from 'semantic-ui-react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -11,14 +12,25 @@ const App = () => {
     <Router>
       <div className="App">
         <header>
-          <NavBar/>
+          <NavBar />
         </header>
         <Switch>
-          <Route exact path="/" component={Menu} />
-          <Route path="/about" component={About}/>
-          <Route path="/contact" component={Contact}/>
+          {/* <Route exact path="/" component={Menu} /> */}
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
         </Switch>
 
+        <footer>
+          <Menu>
+            <Menu.Item disabled>(c) My Copyright</Menu.Item>
+            <Menu.Item position="right">
+              <Input disabled label="Jaime" />
+            </Menu.Item>
+            <Menu.Item>
+              <Input disabled label="Ortiz" />
+            </Menu.Item>
+          </Menu>
+        </footer>
       </div>
     </Router>
   );
