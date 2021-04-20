19d24769951ba96f3177b451d6d6bd548adc53dc
Jaime Ortiz <jxme88@gmail.com>
2021-04-19 19:12:08 -0700

    Playing with semantic ui image reveal, not quite there yet.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "semantic-ui-css/semantic.min.css";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Contact from "./components/Contact";
-// import Menu from "./components/Menu";
-import { Menu, Input } from 'semantic-ui-react'
+import Menu from "./components/Menu";
+// import { Menu, Input } from 'semantic-ui-react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -15,13 +15,13 @@ const App = () => {
           <NavBar />
         </header>
         <Switch>
-          {/* <Route exact path="/" component={Menu} /> */}
+          <Route exact path="/" component={Menu} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
         </Switch>
 
         <footer>
-          <Menu>
+          {/* <Menu>
             <Menu.Item disabled>(c) My Copyright</Menu.Item>
             <Menu.Item position="right">
               <Input disabled label="Jaime" />
@@ -29,7 +29,7 @@ const App = () => {
             <Menu.Item>
               <Input disabled label="Ortiz" />
             </Menu.Item>
-          </Menu>
+          </Menu> */}
         </footer>
       </div>
     </Router>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,48 @@
-const Menu = () => {
-    return (
-        <div>
-            Menu Component. Exact path.
-        </div>
-    )
-}
+import { Grid, Image, Reveal } from "semantic-ui-react";
 
-export default Menu
+const Menu = () => {
+  return (
+    <>
+      <Grid centered fluid vertical columns={1}>
+        <Reveal animated="small fade">
+          <Reveal.Content>
+            <Grid.Row visable>
+              <Image
+                size="medium"
+                src="https://i.pinimg.com/originals/b6/66/17/b66617aa96d478a4bbae6035dd97f308.jpg"
+              />
+            </Grid.Row>
+          </Reveal.Content>
+          <Reveal.Content hidden>
+            <Grid.Row>
+              <Image
+                size="medium"
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+              />
+            </Grid.Row>
+          </Reveal.Content>
+        </Reveal>
+        <Grid.Row>
+          <Image
+            size="medium"
+            src="https://react.semantic-ui.com/images/wireframe/image.png"
+          />
+        </Grid.Row>
+        <Grid.Row>
+          <Image
+            size="medium"
+            src="https://react.semantic-ui.com/images/wireframe/image.png"
+          />
+        </Grid.Row>
+        <Grid.Row>
+          <Image
+            size="medium"
+            src="https://react.semantic-ui.com/images/wireframe/image.png"
+          />
+        </Grid.Row>
+      </Grid>
+    </>
+  );
+};
+
+export default Menu;
