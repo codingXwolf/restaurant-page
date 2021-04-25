94e136720cd7ce32f2f04f4201e0138428489a7c
Jaime Ortiz <jxme88@gmail.com>
2021-04-25 11:15:24 -0700

    Added basic contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,30 @@
+import { Header } from 'semantic-ui-react'
+
 const Contact = () => {
     return (
-        <div>
-            Contact component
-        </div>
-    )
+      <>
+        <Header size="huge" textAlign="center">
+          Contact
+        </Header>
+        <hr style={{ maxWidth: "80%" }} />
+        <Header size="medium" textAlign="center">
+          Phone Number:
+        </Header>
+        <Header size="medium" textAlign="center">
+          <a href="#">626-123-4567</a>
+        </Header>
+        <br/>
+        <Header size="medium" textAlign="center">
+          Address
+        </Header>
+        <Header size="medium" textAlign="center">
+          <a href="#">1234 Rosemead Blvd <br/>
+          Los Angeles, CA 91029
+          </a>
+        </Header>
+
+      </>
+    );
 }
 
 export default Contact
